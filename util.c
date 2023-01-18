10c8cc6725aebfea87547ba2bc5beb3627c65335
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-18 17:03:08 +0100

    push and pall in monty

--- util.c
+++ util.c
@@ -37,19 +37,6 @@ instruction_t *create_instru()
 	ptr[1].opcode = "push", ptr[1].f = push;
 	ptr[2].opcode = "pint", ptr[2].f = pint;
 	ptr[3].opcode = "pop", ptr[3].f = pop;
-	ptr[4].opcode = "swap", ptr[4].f = swap;
-	ptr[5].opcode = "add", ptr[5].f = add;
-	ptr[6].opcode = "nop", ptr[6].f = NULL;
-	ptr[7].opcode = "sub", ptr[7].f = sub;
-	ptr[8].opcode = "div", ptr[8].f = divi;
-	ptr[9].opcode = "mul", ptr[9].f = mul;
-	ptr[10].opcode = "mod", ptr[10].f = mod;
-	ptr[11].opcode = "pchar", ptr[11].f = pchar;
-	ptr[12].opcode = "pstr", ptr[12].f = pstr;
-	ptr[13].opcode = "rotl", ptr[13].f = rotl;
-	ptr[14].opcode = "rotr", ptr[14].f = rotr;
-	ptr[15].opcode = "stack", ptr[15].f = stack;
-	ptr[16].opcode = "queue", ptr[16].f = queue;
 	ptr[17].opcode = NULL, ptr[17].f = NULL;
 
 	return (ptr);
