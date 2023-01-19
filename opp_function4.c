4fff1cbd1f8ab9ea664ce9936f6bd6a6ee061ad8
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-19 22:08:19 +0100

    update 12

--- opp_function4.c
+++ opp_function4.c
@@ -29,18 +29,14 @@ void rotr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
 	(void) line_number;
-
-	if (!stack || !*stack)
+	if (!stack || !*stack || !(*stack)->next)
 		return;
-
 	tmp = *stack;
 	while (tmp->next)
 		tmp = tmp->next;
-
 	tmp->next = *stack;
 	tmp->prev->next = NULL;
 	tmp->prev = NULL;
-
 	(*stack)->prev = tmp;
 	*stack = tmp;
 }
